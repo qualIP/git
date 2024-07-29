560132ac42221791043830c13b4321d923f80bb6
Jean-Sebastien Trottier <jst@qualipsoft.com>
2024-07-28 21:16:00 -0400

    vimdiff: Use GIT_EDITOR environment if it matches the requested tool

--- t/t7609-mergetool--lib.sh
+++ t/t7609-mergetool--lib.sh
@@ -9,7 +9,17 @@ TEST_PASSES_SANITIZE_LEAK=true
 
 test_expect_success 'mergetool --tool=vimdiff creates the expected layout' '
 	. "$GIT_BUILD_DIR"/mergetools/vimdiff &&
-	run_unit_tests
+	run_unit_tests_layout
+'
+
+test_expect_success 'mergetool --tool=vimdiff verification of merge_cmd' '
+	. "$GIT_BUILD_DIR"/mergetools/vimdiff &&
+	run_unit_tests_merge_cmd
+'
+
+test_expect_success 'mergetool --tool=vimdiff creates the expected tool path' '
+	. "$GIT_BUILD_DIR"/mergetools/vimdiff &&
+	run_unit_tests_tool_path
 '
 
 test_done
